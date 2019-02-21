216a70701c72b22bb5333f52aa3b7da388f01a6a
Anton Setzer <a.g.setzer@swansea.ac.uk>
2019-02-21 21:49:03 +0000

    adaption of setupSparkAdaSwansea.sh and accompanying files

--- bin/setupSparkAdaSwansea.sh
+++ bin/setupSparkAdaSwansea.sh
@@ -11,27 +11,26 @@ mkdir -p $HOME/bin
 echo mkdir -p $HOME/git
 mkdir -p $HOME/git
 
-cd $HOME/git; git clone git clone https://anton_setzer@bitbucket.org/anton_setzer/criticalhighintegritysystems.git
+cd $HOME/git; git clone https://anton_setzer@bitbucket.org/anton_setzer/criticalhighintegritysystems.git
 
 exit
 
 # add xml files defining mime types gpr and mlw to ~/.local/share/mime/packages/
-echo wget -P  $shareDir/mime/packages/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/application-gpr.xml
-wget -P  $shareDir/mime/packages/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/application-gpr.xml
+echo cp $HOME/git/lib/application-gpr.xml $shareDir/mime/packages/
+cp $HOME/git/lib/application-gpr.xml $shareDir/mime/packages/
 
-echo wget -P  $shareDir/mime/packages/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/application-mlw.xml
-
-wget -P  $shareDir/mime/packages/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/application-mlw.xml
+echo cp $HOME/git/lib/application-mlw.xml $shareDir/mime/packages/
+cp $HOME/git/lib/application-mlw.xml $shareDir/mime/packages/
 
 
 # add desktop entries for why3 and gps to ~/.local/share/applications/
-echo wget -P  $shareDir/applications/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/callwhy3.desktop
 
-wget -P  $shareDir/applications/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/callwhy3.desktop
+echo cp $HOME/git/lib/callwhy3.desktop $shareDir/applications/
+cp $HOME/git/lib/callwhy3.desktop $shareDir/applications/
 
-echo wget -P  $shareDir/applications/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/callgps.desktop
+echo cp $HOME/git/lib/callgps.desktop $shareDir/applications/
+cp $HOME/git/lib/callgps.desktop $shareDir/applications/
 
-wget -P  $shareDir/applications/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/callgps.desktop
 
 # backup ~/.local/share/applications/mimeapps.list  
 # since it might contain already some user defined mime types
@@ -44,23 +43,25 @@ mv $shareDir/applications/mimeapps.list $shareDir/applications/mimeapps.list.old
 # ~/.local/share/applications/mimeapps.list  
 # by one making associations for .gpr and .mlw
 
-echo wget -P  $shareDir/applications/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/mimeapps.list
 
-wget -P  $shareDir/applications/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/setupfiles/mimeapps.list
+echo cp $HOME/git/lib/mimeapps.list $shareDir/applications/
+cp $HOME/git/lib/mimeapps.list $shareDir/applications/
 
 # add the scripts callgps and callmlw to the user's bin directory
 
 echo rm $HOME/bin/callgps
 rm $HOME/bin/callgps
 
-echo wget -P  $HOME/bin/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/callgps
-wget -P  $HOME/bin/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/callgps
+echo cp $HOME/git/bin/callgps $HOME/bin
+cp $HOME/git/bin/callgps $HOME/bin
 
 echo rm $HOME/bin/callwhy3 
 rm $HOME/bin/callwhy3 
 
-echo wget -P  $HOME/bin/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/callwhy3
-wget -P  $HOME/bin/ http://www.cs.swan.ac.uk/~csetzer/lectures/critsys/current/SPARK_Ada/files/callwhy3
+echo cp $HOME/git/bin/callwhy3 $HOME/bin
+cp $HOME/git/bin/callwhy3 $HOME/bin
+
+
 
 echo chmod u+x $HOME/bin/callgps
 chmod u+x $HOME/bin/callgps
