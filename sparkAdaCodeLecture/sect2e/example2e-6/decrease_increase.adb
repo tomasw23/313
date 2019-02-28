482baecd2710b23fca692e24952c3353603c3e8d
Anton Setzer <a.g.setzer@swansea.ac.uk>
2019-02-28 23:08:07 +0000

    minor change example2e-6

--- sparkAdaCodeLecture/sect2e/example2e-6/decrease_increase.adb
+++ sparkAdaCodeLecture/sect2e/example2e-6/decrease_increase.adb
@@ -3,7 +3,7 @@ pragma SPARK_Mode;
 procedure Decrease_Increase(X,Y : in out Integer; Z : in Integer) is
 begin
   loop
-     exit when Z < 0 or X < 0;
+     exit when Z < 0 or X <= 0;
      X := X - 1; Y := Y + 1;
   end loop;
 end Decrease_Increase;
