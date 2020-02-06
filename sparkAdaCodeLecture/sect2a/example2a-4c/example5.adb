3cc12e56aa92b71ed9fae671a5b4285e7370dbfe
Anton Setzer <a.g.setzer@swansea.ac.uk>
2020-02-06 13:26:33 +0000

    addition of sect2a, example2a-4c and example2a-4d regarding subtype vs type

---
+++ sparkAdaCodeLecture/sect2a/example2a-4c/example5.adb
@@ -0,0 +1,25 @@
+with Ada.Text_Io, Ada.Integer_Text_IO;
+use Ada.Text_Io, Ada.Integer_Text_Io ;
+
+
+Procedure Example5 is
+   subtype Year is Integer range 0 .. 2100;
+   Integervar   : Integer := 2010;
+   Yearvar  : Year;
+  begin
+     Put("Year = ");
+     Get(Integervar);
+     -- Put(Myyear);
+     -- Myyear := Myyear + 1;
+     -- Put(Myyear);
+     if Integervar >= 0 and Integervar <= 2100 then
+	   Yearvar := Year(Integervar);
+     else
+	   Yearvar := 2100;
+     end if;
+     Integervar  := Yearvar;
+     
+  end Example5;
+  
+     
+	    
